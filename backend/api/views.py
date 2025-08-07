7d5fcad3057061cf29ea41d85154a60539fe97e8
alex <aleksey.zavadskiy@gmail.com>
2025-08-07 12:48:41 +0300

    -

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
 from .models import Note
 from .serializer import NoteSerializer
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
 @api_view(['GET'])
